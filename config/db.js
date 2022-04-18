a0527d5efa4180f67f93d80b81fe1eefb4d90992
Yunus <yolgan@gmail.com>
2022-04-18 12:20:21 +0300

    add

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = ()=>{
     
     // mongoose.connect(`mongodb+srv://yunus:${process.env.APIKEY}@cluster0.kx7vo.mongodb.net/myFirstDatabase`);
-    mongoose.connect(`mongodb+srv://${process.env.APIKEY}@cluster0.kx7vo.mongodb.net/candidates`,
+    mongoose.connect(`mongodb+srv://${process.env.APIKEY}@cluster0.kx7vo.mongodb.net/candidates?retryWrites=true&w=majority`,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
